f851daaf1e366860258ae6623a92cbf008a0b232
oklaura <oklaura@protonmail.com>
2021-08-09 17:20:57 -0400

    ends round when word is completed

--- classes/dictionary.rb
+++ classes/dictionary.rb
@@ -9,13 +9,36 @@ class Dictionary
         @dictionary = Set.new(words)
     end
 
-    def self.valid?(letter)
+    def self.valid_letter?(letter)
         if ALPHABET.include?(letter)
+            return true
+        else
+            puts "invalid character"
+        end
+        false
+    end
+
+    def valid_fragment?(fragment, letter)
+        new_fragment = fragment + letter
+        if @dictionary.any? {|word| word.start_with?(new_fragment)}
             return true
         else
             puts "invalid fragment"
         end
-        return false
+        false
+    end
+
+    def complete_word?(fragment, letter="")
+        if fragment + letter == ""
+            return false
+        end
+        
+        new_fragment = fragment + letter
+        if @dictionary.include?(new_fragment)
+            return true
+        else
+            return false
+        end
     end
 
 
--- classes/game.rb
+++ classes/game.rb
@@ -13,10 +13,11 @@ class Game
 
     def initialize(*players)
         @players = players
-        losses = {}
+        @losses = {}
         @players.each {|player| losses[player] = 0}
         @current_player = @players[0]
         @fragment = ''
+        @dictionary = Dictionary.new
     end
 
     def run
@@ -28,15 +29,25 @@ class Game
     end
 
     def play_round
+
+        @fragment = ''
+        read_scores
+
+        until @dictionary.complete_word?(@fragment)
         player = @current_player
         letter = player.get_letter
-        if Dictionary.valid?(letter)
+        if Dictionary.valid_letter?(letter) && @dictionary.valid_fragment?(@fragment, letter) && !@dictionary.complete_word?(@fragment, letter)
             add_to_fragment(letter)
+        elsif @dictionary.complete_word?(@fragment, letter)
+            add_to_fragment(letter)
+            update_losses
+            return
         else
             return
         end
         puts "Word fragment: " + @fragment
         switch_player
+        end
     end
 
     def add_to_fragment(letter)
@@ -65,12 +76,34 @@ class Game
         end
     end
 
+    def update_losses
+        # losses[@current_player] += 1
+        # if losses[@current_player] == MAX_LOSSES
+            puts "#{@current_player.name} spelled 'GHOST' and is eliminated from the game"
+        # else
+        #     puts "#{@current_player.name} loses round and get's a letter. Score: #{spell_ghost(@losses[@current_player])}"
+        # end
+    end
+
+    def spell_ghost(losses)
+        word = "Ghost"
+        score = ""
+        (0..losses-1).each {|i| score += word[i]}
+        score
+    end
+
+    def read_scores 
+        @losses.each do |k, v|
+            puts "#{k.name}: #{spell_ghost(v)}"
+        end
+    end
+
 
 
 end
 
 
-game = Game.new(Player.new('Jordan'), Player.new('Liz'))
+
 
 
 
